bd0002b0d1b547bd58827a629bd92ffb13269fa2
Adrian Medina <adrian.medina@mail.yu.edu>
2014-12-04 13:57:56 -0500

    begin random account creation logic

--- src/blackout/core.clj
+++ src/blackout/core.clj
@@ -1,1 +1,36 @@
-(ns blackout.core)
+(ns blackout.core
+  (:require [aleph.http :as http]
+            [manifold.deferred :as d]
+            [byte-streams :as bs]
+            [clojure.core.async :refer :all
+             :exclude [map into reduce merge take partition partition-by]]
+            [cheshire.core :as json]))
+
+(def ^:dynamic *host* "localhost")
+(def ^:dynamic *port* 8080)
+
+(defn make-uri
+  [route]
+  (str "http://" *host* *port* route))
+
+(defn post
+  [route body]
+  (http/post (make-uri route)
+             {:content-type :json
+              :accept :json
+              :body (json/generate-string body)}))
+
+(defn create-account
+  []
+  (post "/api/v1/accounts" {:action "create"
+                            :args {}
+                            :groups {}
+                            :password ""}))
+
+(defn -main
+  [& {:keys [host port]}]
+  (binding [*host* host
+            *port* port]
+    ))
+
+
