427ecccaae87a629b9c79c765398433e4094f038
Adrian Medina <adrian.medina@mail.yu.edu>
2014-12-04 16:10:54 -0500

    basic request stat aggregator

--- src/blackout/core.clj
+++ src/blackout/core.clj
@@ -11,6 +11,7 @@
 
 (def ^:dynamic *account* nil)
 (def ^:dynamic *session-cookie* nil)
+(def ^:dynamic *accounts* nil)
 
 (defn make-uri
   [route]
@@ -46,14 +47,45 @@
                                 :password password
                                 :app 1}}))
 
+(defn gen-agents
+  []
+  (repeatedly (.availableProcessors (Runtime/getRuntime)) #(agent [])))
+
+(defn aggregate-request-stats
+  [stats res]
+  (let [stats (cond-> (update stats :num-requests inc)
+                (== (:status res) 200)
+                (update :num-success inc)
+
+                (not= (:status res) 200)
+                (update :num-error inc))]
+    (-> stats
+        (assoc :success-rate (/ (:num-success stats) (:num-requests stats))))))
+
 (defn -main
   [& args]
   (binding [*account* nil
-            *session-cookie* nil]
+            *session-cookie* nil
+            *accounts* (atom #{})]
     (let [res @(login (env :switchboard-admin-username)
                       (env :switchboard-admin-password))
-          body (slurp (:body res))]
+          body (slurp (:body res))
+          agents (gen-agents)]
       (assert (== (:status res) 200) body)
       (set! *account* (:result (json/parse-string body true)))
       (set! *session-cookie* (get (:headers res) :set-cookie))
-      (log/info (slurp (:body @(create-account)))))))
+
+      (loop [[a & more] (cycle agents)
+             responses []]
+        (if (< (count responses) 32)
+          (let [res (promise)]
+            (send a (fn [_] (deliver res @(create-account))))
+            (recur more (conj responses res)))
+          (transduce (comp (map deref))
+                     (completing aggregate-request-stats)
+                     {:total-time 0
+                      :mean-latency 0
+                      :num-success 0
+                      :num-error 0
+                      :num-requests 0
+                      :success-rate 1} responses))))))
