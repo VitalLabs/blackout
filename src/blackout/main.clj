a58eaaa15b04d633da78e0c5a8d897b15401e0ce
Adrian Medina <adrian.medina@mail.yu.edu>
2014-12-10 13:21:50 -0500

    set the java classpath when invoking riemann through the shell env

--- src/blackout/main.clj
+++ src/blackout/main.clj
@@ -1,11 +1,18 @@
 (ns blackout.main
   (:gen-class)
-  (:require [clojure.java.shell :refer [sh]]))
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.java.io :as io]))
 
 (defn classpath
   []
-  (System/getProperty "java.class.path"))
+  {:CLASSPATH (System/getProperty "java.class.path")})
 
 (defn -main
   [& args]
-  (println (classpath)))
+  (if-let [cfg (io/resource "riemann.config")]
+    (let [result (sh "riemann" (.getPath cfg) :env (classpath))]
+      (if-not (empty? (:err result))
+        (println (:err result))
+        (println (:out result))))
+    (throw (ex-info "riemann.config not found in classpath" {})))
+  (System/exit 0))
