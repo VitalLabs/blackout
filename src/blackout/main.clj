9a3bbfe3176efa36b635516c8c7a66a6049d4ec1
Adrian Medina <adrian.medina@mail.yu.edu>
2014-12-10 13:33:03 -0500

    :CLASSPATH => "CLASSPATH"

--- src/blackout/main.clj
+++ src/blackout/main.clj
@@ -1,18 +1,19 @@
 (ns blackout.main
   (:gen-class)
-  (:require [clojure.java.shell :refer [sh]]
+  (:require [clojure.java.shell :refer [sh with-sh-env]]
             [clojure.java.io :as io]))
 
 (defn classpath
   []
-  {:CLASSPATH (System/getProperty "java.class.path")})
+  {"CLASSPATH" (System/getProperty "java.class.path")})
 
 (defn -main
   [& args]
   (if-let [cfg (io/resource "riemann.config")]
-    (let [result (sh "riemann" (.getPath cfg) :env (classpath))]
-      (if-not (empty? (:err result))
-        (println (:err result))
-        (println (:out result))))
+    (with-sh-env (classpath)
+      (let [result (sh "riemann" (.getPath cfg))]
+        (if-not (empty? (:err result))
+          (println (:err result))
+          (println (:out result)))))
     (throw (ex-info "riemann.config not found in classpath" {})))
   (System/exit 0))
